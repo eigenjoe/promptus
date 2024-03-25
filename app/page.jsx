042490eb100f1223ac8a8b1aa9aa86884543373a
eigenjoe <eigenjosephmendoza@gmail.com>
2024-03-25 14:15:53 -0400

    Added basic stylings to page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,14 @@
 
  const Home = () => {
   return (
-    <div>
-        Home
-    </div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Share & Discover        
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center"> AI prompts for everyone! </span>
+        </h1>
+ 
+        <p className="sub_text text-center">Promptus is a simple and elegant way to create and share AI prompts.</p>
+    </section>
   )
 }
 
