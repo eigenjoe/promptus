1b04817590cd73d945e715052cefa983f94e3693
eigenjoe <eigenjosephmendoza@gmail.com>
2024-03-26 06:39:04 -0400

    Added google auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,28 +2,29 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import "@styles/globals.css";
 
-
 export const metadata = {
-    title: "Promptus",
-    description: "Promptus is a simple and elegant way to create and share AI prompts.",
-}
+	title: "Promptus",
+	description:
+		"Promptus is a simple and elegant way to create and share AI prompts.",
+};
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang="en">
-        <body>
-            <div className="main">
-                < div className="gradient" />
-            </div>
+const RootLayout = ({ children }) => {
+	return (
+		<html lang="en">
+			<body>
+				<Provider>
+					<div className="main">
+						<div className="gradient" />
+					</div>
 
-            <main className="app">
-                <Nav />
-                { children } 
-            </main>
-        </body>
-
-    </html>
-  )
-}
+					<main className="app">
+						<Nav />
+						{children}
+					</main>
+				</Provider>{" "}
+			</body>
+		</html>
+	);
+};
 
 export default RootLayout;
