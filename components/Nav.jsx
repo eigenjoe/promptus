916318e623ac525a7ff1801cb12dfca4c933e45c
eigenjoe <eigenjosephmendoza@gmail.com>
2024-03-26 04:43:20 -0400

    Started working on Navigation bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client"
+import Link from "next/link"    
+import Image from "next/image"
+import { useState, useEffect } from "react"
+import { signIn, signOut, useSession, getProviders } from "next-auth/react"
+import logo from "@public/images/logo.jpg"
+
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href={'/'} className="flex gap-2 flex-center">
+            <Image src={logo} alt="Promptus Logo" width={50} height={50} />
+        </Link>
+    </nav>
   )
 }
 
